2c17eb742704fe6e1edb44b35170d092df8a6f1d
challfuture <challfuture@gmail.com>
2016-12-01 08:58:56 -0500

    New file on the git

---
+++ java/myscript.js
@@ -0,0 +1,33 @@
+
+
+$(function(){
+	
+	"use strict";
+	var topoffset = 50; // varibale for menu height
+	
+// 	Active Scrollspy
+
+$('body').scrollspy ({
+	target: 'header .navbar',
+	offset: topoffset
+});
+
+	
+
+
+
+
+
+
+
+
+	
+$('.carousel').carousel({
+  interval: flase
+});
+	
+	
+
+	
+	
+});
